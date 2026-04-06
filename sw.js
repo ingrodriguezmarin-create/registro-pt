74a9b52a3f2124955485b6020fa1ef81714e3dc9
ingrodriguezmarin-create <ing.rodriguezmarin@gmail.com>
2026-04-05 20:58:17 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,47 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+const CACHE_NAME = 'regispt-v2';
+
+const urlsToCache = [
+  './',
+  'index.html',
+  'manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://actions.google.com/sounds/v1/alarms/spaceship_alarm.ogg',
+  'https://cdn-icons-png.flaticon.com/512/1082/1082810.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Cache abierta: guardando archivos para modo offline');
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
 self.addEventListener('notificationclick', (e) => {
   e.notification.close();
